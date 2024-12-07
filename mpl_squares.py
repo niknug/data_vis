6c46a20aa373149a831503c94dc2ed74141ad45b
niknug <niknug@yandex.ru>
2024-12-07 04:09:45 +0300

    Added simple diagram

---
+++ mpl_squares.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3, color='red')
+
+ax.set_title('Square Numbers', fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Value', fontsize=14)
+
+ax.tick_params(axis='both', labelsize=14)
+
+plt.show()
