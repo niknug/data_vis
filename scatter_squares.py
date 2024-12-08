c6af5fde34cd017e1bd526f6d833aebfe33f1ac7
niknug <niknug@yandex.ru>
2024-12-09 01:48:30 +0300

    Added saving to file

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,12 +2,12 @@ from shutil import which
 
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y_values = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+x_values = list(range(1, 1001))
+y_values = [x ** 2 for x in x_values]
 
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=5, c=y_values, cmap=plt.cm.Blues)
 
 ax.set_title('Square Numbers', fontsize=24)
 ax.set_xlabel('Value', fontsize=14)
@@ -15,4 +15,7 @@ ax.set_ylabel('Square of Value', fontsize=14)
 
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-plt.show()
+ax.axis([0, 1100, 0, 1100000])
+
+# plt.show()
+plt.savefig('squares.png', bbox_inches='tight')
