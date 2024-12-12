67e2d1733fb005855cdd27e9120dca4fa4bfe360
niknug <niknug@yandex.ru>
2024-12-12 23:13:16 +0300

    Added bar chart

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 die = Die()
@@ -8,4 +11,17 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Results', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({"data": data, "layout": my_layout}, filename='d6.html')
