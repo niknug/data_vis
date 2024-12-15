315156a8047805c26948d8779196a617cac8e5d5
niknug <niknug@yandex.ru>
2024-12-16 01:30:25 +0300

    Added temperatures diagram

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,7 @@
 import csv
 
+from matplotlib import pyplot as plt
+
 filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -14,4 +16,13 @@ with open(filename) as f:
         high = int(row[5])
         highs.append(high)
 
-print(highs)
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+plt.title("Daily High Temperatures, July 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
